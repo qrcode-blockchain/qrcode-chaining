14af3c6ced580461c76491fdaaa1ccc9642cd992
Keith-Zidan-25 <keithzidand@gmail.com>
2025-05-20 00:00:18 +0530

    Final fixes made

--- app/api/product/[productIdentifier]/route.js
+++ app/api/product/[productIdentifier]/route.js
@@ -2,6 +2,7 @@ import { dbConnect } from "../../../../lib/dbConnect";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import Product from "../../../../model/Product";
+import { error } from "console";
 
 export async function GET(request, { params }) {
     const {productIdentifier} = await params;
@@ -52,9 +53,23 @@ export async function GET(request, { params }) {
                 },
             },
         ]);
+
+        if (!result) {
+            return NextResponse.json(
+                {success: false, errorMsg: 'Product Not found'},
+                {status: 404}
+            )
+        }
+
+        let productData = result[0];
+        const createdAtDate = productData['date'];
+        const formattedDate = createdAtDate.getFullYear().toString() + '-' +
+                                                    String(createdAtDate.getMonth() + 1).padStart(2, '0') + '-' +
+                                                    String(createdAtDate.getDate()).padStart(2, '0');
+        productData['date'] = formattedDate
         
         return NextResponse.json(
-            {success: true, product: result[0]},
+            {success: true, product: productData},
             {status: 200}
         )
         
--- app/products/[name]/[location]/[date]/[batchNo]/[serialNo]/[id]/page.jsx
+++ app/products/[name]/[location]/[date]/[batchNo]/[serialNo]/[id]/page.jsx
@@ -90,16 +90,12 @@ export default function ProductPage({ params }) {
                 <h1 className="text-2xl font-bold text-white">
                 Product Details
                 </h1>
-                <Image src="/msb.jpg" height={100} width={100}/>
                 
             </div>
             </div>
 
                 <div className="bg-gray-50 px-6 py-4 border-t border-gray-200">
                     <div className="flex items-center justify-between">
-                    <span className="text-sm text-gray-500">
-                        Blockchain Verified
-                    </span>
                     <span className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-green-100 text-green-800">
                         ✓ Verified
                     </span>
